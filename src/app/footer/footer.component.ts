2a7165ea470ffb6c82bb72592f53839fc43b0d74
Anku Garg <ankugarg580@gmail.com>
2019-03-18 12:46:03 +0530

    Added the cart

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -7,9 +7,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FooterComponent implements OnInit {
 
+  orders = [
+    {
+      dish_id : "1",
+      dish_name : "Chapati",
+      quantity : 1,
+    },
+    {
+      dish_id : "2",
+      dish_name : "Chapati",
+      quantity : 1,
+    },
+    {
+      dish_id : "3",
+      dish_name : "Chapati",
+      quantity : 1,
+    }
+  ]
   constructor() { }
 
   ngOnInit() {
   }
 
+  quantityIncrement(id : any){
+    let i = 0;
+    for(i=0; i<this.orders.length; i++) {
+      if(this.orders[i].dish_id == id) {
+        this.orders[i].quantity = this.orders[i].quantity + 1;
+      }
+    }
+  }
+
+  quantityDecrement(id : any){
+    let i = 0;
+    for(i=0; i<this.orders.length; i++) {
+      if(this.orders[i].dish_id == id) {
+        this.orders[i].quantity = this.orders[i].quantity - 1;
+      }
+    }
+  }
 }
