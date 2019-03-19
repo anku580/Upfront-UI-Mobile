9a0291286635678d7f2eb319cb8acdc054966c48
Anku Garg <ankugarg580@gmail.com>
2019-03-19 15:52:44 +0530

    Added the Restaurant Details

--- src/app/restaurant-detail/restaurant-detail.component.ts
+++ src/app/restaurant-detail/restaurant-detail.component.ts
@@ -14,24 +14,36 @@ export class RestaurantDetailComponent implements OnInit {
     distance : 3.2,
   }
 
+  private dishSelected = false;
+  private Quantity;
+
   private restaurantDetail = [
     {
       category : "Starters",
       dishes : [
         {
+          dish_id : 1,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         },
         {
+          dish_id : 2,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         },
         {
+          dish_id : 3,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         }
       ]
     },
@@ -39,19 +51,28 @@ export class RestaurantDetailComponent implements OnInit {
       category : "Main Course",
       dishes : [
         {
+          dish_id : 4,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         },
         {
+          dish_id : 5,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         },
         {
+          dish_id : 6,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         }
       ]
     },
@@ -59,19 +80,28 @@ export class RestaurantDetailComponent implements OnInit {
       category : "Dessert",
       dishes : [
         {
+          dish_id : 7,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         },
         {
+          dish_id : 8,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         },
         {
+          dish_id : 9,
           dish_name : "Chicken soup",
           dish_price : 120,
-          imgURL : "abc"
+          imgURL : "abc",
+          quantity : 0,
+          is_selected : false
         }
       ]
     }
@@ -79,6 +109,41 @@ export class RestaurantDetailComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // console.log(this.restaurantDetail[0].dishes)
+  }
+
+  removeSelected(){
+    return false;
+  }
+
+  dishAddedToCart(id) {
+    console.log("Clicked: ", id);
+    this.dishSelected = true;
+  }
+
+  quantityIncrement(id : any){
+    let i = 0,j = 0;
+    for(i=0; i<this.restaurantDetail.length; i++) {
+      for(j=0; j<this.restaurantDetail[i].dishes.length; j++) {
+        if(this.restaurantDetail[i].dishes[j].dish_id == id) {
+          this.restaurantDetail[i].dishes[j].quantity = this.restaurantDetail[i].dishes[j].quantity + 1;
+        }
+      }
+    }
+  }
+
+  quantityDecrement(id : any){
+    let i = 0,j = 0;
+    for(i=0; i<this.restaurantDetail.length; i++) {
+      for(j=0; j<this.restaurantDetail[i].dishes.length; j++) {
+        if(this.restaurantDetail[i].dishes[j].dish_id == id) {
+          this.restaurantDetail[i].dishes[j].quantity = this.restaurantDetail[i].dishes[j].quantity - 1;
+          if(this.restaurantDetail[i].dishes[j].quantity < 1) {
+            this.dishSelected = false
+          }
+        }
+      }
+    }
   }
 
 }
